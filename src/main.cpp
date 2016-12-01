94d41dfd7db8cbc763084c320f8a2e9654174934
Vesso <vesselin@vv-studio.com>
2016-12-01 15:44:31 +0100

    Update main.cpp for Mac/oF 8.1+
    Fixed the furry shader compiling in oF. (Mac)

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include "ofAppGlutWindow.h"
 
 int main(){
-	ofAppGlutWindow window; 
-	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
-	ofRunApp(new kinectfurry());
+    ofGLWindowSettings settings;
+    settings.setGLVersion(4,1); /// < select your GL Version here for furrykinect minimum is 3.1 (Important for mac, because otherwise it doesn't build with the selected version by default) 
+    ofCreateWindow(settings); ///< create your window here
+    ofRunApp(new kinectfurry());
 }
